1072063db4392b090c5bc8563d72279e0846c2ee
icepick4 <remi.jara@laposte.net>
2023-12-14 15:11:55 +0100

    initiate angular project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AddUserComponent } from './users/add-user/add-user.component';
+import { SingleUserComponent } from './users/single-user/single-user.component';
+import { UpdateUserComponent } from './users/update-user/update-user.component';
+import { UserListComponent } from './users/user-list/user-list.component';
+
+const routes: Routes = [
+  { path: 'users', component: UserListComponent },
+  { path: 'users/:id', component: SingleUserComponent },
+  { path: 'users/add', component: AddUserComponent },
+  { path: 'users/update/:id', component: UpdateUserComponent },
+  { path: '', component: HomeComponent },
+  { path: '**', redirectTo: 'users' },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    SingleUserComponent,
+    AddUserComponent,
+    UpdateUserComponent,
+    HomeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
