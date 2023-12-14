35e42994414f479c631db2efa4645ec56e8f3a53
Rémi JARA <82316285+icepick4@users.noreply.github.com>
2023-12-14 15:03:01 +0000

    adding user list base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
@@ -32,6 +39,13 @@ const routes: Routes = [
     BrowserModule,
     BrowserAnimationsModule,
     RouterModule.forRoot(routes),
+    MatIconModule,
+    MatSortModule,
+    MatButtonModule,
+    MatPaginatorModule,
+    MatInputModule,
+    FormsModule,
+    MatTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/users/user-list/user-list.component.ts
+++ src/app/users/user-list/user-list.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css']
+  styleUrls: ['./user-list.component.css'],
 })
-export class UserListComponent {
+export class UserListComponent implements OnInit {
+  users!: MatTableDataSource<User>;
 
+  displayedColumns: string[] = ['name', 'email', 'occupation'];
+
+  filterValue = '';
+
+  applyFilter() {
+    this.users.filter = this.filterValue.trim().toLowerCase();
+  }
+
+  ngOnInit(): void {
+    let users: User[] = [
+      {
+        id: 1,
+        name: 'John Doe',
+        email: 'zzzz',
+        bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptas.',
+        occupation: 'Web Developer',
+      },
+      {
+        id: 2,
+        name: 'Jane Doe',
+        email: 'aaaa',
+        bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptas.',
+        occupation: 'Web Designer',
+      },
+    ];
+
+    this.users = new MatTableDataSource(users);
+
+    this.users.filterPredicate = function (
+      data: User,
+      filter: string
+    ): boolean {
+      return data.email.toLowerCase().includes(filter);
+    };
+  }
 }
